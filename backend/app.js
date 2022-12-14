60d4bd21d097e9b83c3034df5f0382cbccfd5c16
Anthony Licas <anthonylicas@gmail.com>
2022-12-14 12:52:23 -0800

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
 const { environment } = require('./config');
+const { ValidationError } = require('sequelize');
 const isProduction = environment === 'production';
 
 const app = express();
@@ -40,9 +41,37 @@ if (!isProduction) {
   );
 
 
-
-
-// ...
-
 app.use(routes); // Connect all the routes
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
 module.exports = app;
