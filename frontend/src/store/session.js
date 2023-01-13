cd5930743737c83f02499c01beb69740d06e217a
Anthony Licas <anthonylicas@gmail.com>
2023-01-13 13:10:40 -0800

    completed signup and logout

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -4,6 +4,7 @@ import { csrfFetch } from './csrf';
 const SET_USER = 'session/setUser';
 const REMOVE_USER = 'session/removeUser';
 
+
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -15,6 +16,32 @@ const removeUser = () => {
   return {
     type: REMOVE_USER,
   };
+};
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
 };
 
 export const restoreUser = () => async dispatch => {
